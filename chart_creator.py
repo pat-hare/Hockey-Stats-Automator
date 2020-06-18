a9e5ff61e4522c8840c29656573318b56bfdc6cb
Patrick Hare <patrickhare42@gmail.com>
2020-06-18 21:01:44 +0100

    refactoring and fixed graphs

---
+++ chart_creator.py
@@ -0,0 +1,202 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def createBasicChart(home_data, away_data):
+    df = pd.DataFrame(
+        {
+            'OGHC': [
+                sum(home_data['ce'].values()),
+                sum(home_data['25e'].values()),
+                home_data['pc_win'],
+                home_data['gso'],
+                home_data['shots'],
+                home_data['goals'],
+            ],
+            'Reading': [
+                sum(away_data['ce'].values()),
+                sum(away_data['25e'].values()),
+                away_data['pc_win'],
+                away_data['gso'],
+                away_data['shots'],
+                away_data['goals'],
+            ],
+        }, 
+        index = [
+            'Circle Entries',
+            '25 Entries',
+            'PC Wins',
+            'GSO',
+            'Shots',
+            'Goals',
+        ]
+    )
+
+    stacked_data = df.apply(lambda x: x*100/sum(x), axis=1)
+    stacked_data = stacked_data.plot.barh(stacked=True)
+    stacked_data.axes.xaxis.set_visible(False)
+    plt.gca().legend(loc='upper center', bbox_to_anchor=(0.5,-0.05), ncol=2)
+
+    plt.text(x=1,y=4.9, s=home_data['goals'], size=12, color='white')
+    plt.text(x=1,y=3.9, s=home_data['shots'], size=12, color='white')
+    plt.text(x=1,y=2.9, s=home_data['gso'], size=12, color='white')
+    plt.text(x=1,y=1.9, s=home_data['pc_win'], size=12, color='white')
+    plt.text(x=1,y=0.9, s=sum(home_data['25e'].values()), size=12, color='white')
+    plt.text(x=1,y=-0.1, s=sum(home_data['ce'].values()), size=12, color='white')
+
+    plt.text(x=95,y=4.9, s=away_data['goals'], size=12, color='white')
+    plt.text(x=95,y=3.9, s=away_data['shots'], size=12, color='white')
+    plt.text(x=95,y=2.9, s=away_data['gso'], size=12, color='white')
+    plt.text(x=95,y=1.9, s=away_data['pc_win'], size=12, color='white')
+    plt.text(x=95,y=0.9, s=sum(away_data['25e'].values()), size=12, color='white')
+    plt.text(x=95,y=-0.1, s=sum(away_data['ce'].values()), size=12, color='white')
+
+    return stacked_data
+
+def createTimeSeriesChart(home_rows, away_rows):
+    home_times = []
+    away_times = []
+    home_chart_scores = []
+    away_chart_scores = []
+
+    for row in home_rows:
+        time = row['end']
+        time = round(time, 1)
+
+        score = 0
+
+        if {'name':'25 Entry (L)'} in row['events']:
+            score += 2
+        elif {'name':'25 Entry (C)'} in row['events']:
+            score += 2
+        elif {'name':'25 Entry (R)'} in row['events']:
+            score += 2
+        
+        if {'name':'CE 1'} in row['events']:
+            score += 4
+        elif {'name':'CE 2'} in row['events']:
+            score += 4
+        elif {'name':'CE 3'} in row['events']:
+            score += 4
+        elif {'name':'CE 4'} in row['events']:
+            score += 4
+        elif {'name':'CE 5'} in row['events']:
+            score += 4
+        
+        if {'name':'GSO'} in row['events']:
+            score += 6
+        if {'name':'Shot'} in row['events']:
+            score += 6
+        if {'name':'PC Win'} in row['events']:
+            score += 6
+        if {'name':'PP Win'} in row['events']:
+            score += 2
+        if {'name':'Lost'} in row['events']:
+            score -= 1
+
+        home_times.append(time)
+        home_chart_scores.append(score)
+
+    for row in away_rows:
+        time = row['end']
+        time = round(time, 1)
+
+        score = 0
+
+        if {'name':'25 Entry (L)'} in row['events']:
+            score += 2
+        elif {'name':'25 Entry (C)'} in row['events']:
+            score += 2
+        elif {'name':'25 Entry (R)'} in row['events']:
+            score += 2
+        
+        if {'name':'CE 1'} in row['events']:
+            score += 4
+        elif {'name':'CE 2'} in row['events']:
+            score += 4
+        elif {'name':'CE 3'} in row['events']:
+            score += 4
+        elif {'name':'CE 4'} in row['events']:
+            score += 4
+        elif {'name':'CE 5'} in row['events']:
+            score += 4
+        
+        if {'name':'GSO'} in row['events']:
+            score += 6
+        if {'name':'Shot'} in row['events']:
+            score += 6
+        if {'name':'PC Win'} in row['events']:
+            score += 6
+        if {'name':'PP Win'} in row['events']:
+            score += 2
+        if {'name':'Lost'} in row['events']:
+            score -= 1
+
+        away_times.append(time)
+        away_chart_scores.append(score)
+
+
+    hdf = pd.DataFrame({'time': home_times, 'home scores': home_chart_scores})
+    hdf['time'] = hdf['time'].div(60).round(2)
+    hdf = hdf.set_index('time')
+    hdf = hdf.cumsum()
+
+    adf = pd.DataFrame({'time': away_times, 'away scores': away_chart_scores})
+    adf['time'] = adf['time'].div(60).round(2)
+    adf = adf.set_index('time')
+    adf = adf.cumsum()
+
+    bigdata = hdf.append(adf)
+
+    bigdata.plot.line()
+
+    return bigdata
+
+def createPossessionTimeScoreChart(rows):
+    scores = []
+
+    for row in rows:
+        time = row['end'] - row['start']
+        time = round(time, 1)
+
+        score = 0
+
+        if {'name':'25 Entry (L)'} in row['events']:
+            score += 2
+        elif {'name':'25 Entry (C)'} in row['events']:
+            score += 2
+        elif {'name':'25 Entry (R)'} in row['events']:
+            score += 2
+        
+        if {'name':'CE 1'} in row['events']:
+            score += 4
+        elif {'name':'CE 2'} in row['events']:
+            score += 4
+        elif {'name':'CE 3'} in row['events']:
+            score += 4
+        elif {'name':'CE 4'} in row['events']:
+            score += 4
+        elif {'name':'CE 5'} in row['events']:
+            score += 4
+        
+        if {'name':'GSO'} in row['events']:
+            score += 6
+        if {'name':'Shot'} in row['events']:
+            score += 6
+        if {'name':'PC Win'} in row['events']:
+            score += 6
+        if {'name':'PP Win'} in row['events']:
+            score += 2
+        if {'name':'Lost'} in row['events']:
+            score -= 1
+
+        scores.append((time,score))
+
+    scores.sort()
+
+    df = pd.DataFrame(scores,columns=['time','score'])
+    df = df.set_index('time')
+    print(df)
+    chart = df.plot.bar()
+
+    return chart
+    
---
+++ metrics_calc.py
@@ -0,0 +1,135 @@
+
+def basic_metrics_calc(rows, goals):
+    basic_metrics = {
+        'possession_time': 0,
+        'goals': goals,
+        'gso': 0,
+        'shots': 0,
+        'pc_win': 0,
+        'pp_win': 0,
+        'lost': 0,
+        'ce': {
+            '1': 0,
+            '2': 0,
+            '3': 0,
+            '4': 0,
+            '5': 0,
+        },
+        '25e': {
+            'L': 0,
+            'C': 0,
+            'R': 0,
+        },
+        'xG': 0,
+        'xS': 0,
+        'xPCG': 0,
+        'xPC': 0,
+        'oEff': 0,
+        '25Eff': 0,
+        'ceEff': 0 
+    }
+
+    for row in rows:
+        if {'name':'25 Entry (L)'} in row['events']:
+            basic_metrics['25e']['L'] += 1
+        elif {'name':'25 Entry (C)'} in row['events']:
+            basic_metrics['25e']['C'] += 1
+        elif {'name':'25 Entry (R)'} in row['events']:
+            basic_metrics['25e']['R'] += 1
+        
+        if {'name':'CE 1'} in row['events']:
+            basic_metrics['ce']['1'] += 1
+        elif {'name':'CE 2'} in row['events']:
+            basic_metrics['ce']['2'] += 1
+        elif {'name':'CE 3'} in row['events']:
+            basic_metrics['ce']['3'] += 1
+        elif {'name':'CE 4'} in row['events']:
+            basic_metrics['ce']['4'] += 1
+        elif {'name':'CE 5'} in row['events']:
+            basic_metrics['ce']['5'] += 1
+        
+        if {'name':'GSO'} in row['events']:
+            basic_metrics['gso'] += 1
+        if {'name':'Shot'} in row['events']:
+            basic_metrics['shots'] += 1
+        if {'name':'PC Win'} in row['events']:
+            basic_metrics['pc_win'] += 1
+        if {'name':'PP Win'} in row['events']:
+            basic_metrics['pp_win'] += 1
+        if {'name':'Lost'} in row['events']:
+            basic_metrics['lost'] += 1
+
+    return basic_metrics
+
+def pp_metrics_calc(rows):
+    pp_metrics = {
+        'total': 0,
+        'forwards': 0,
+        'backwards': 0,
+        'ce': 0,
+        'gso': 0,
+        'pc_win': 0,
+        'goals': 0,
+        'lost': 0,
+        'pp_win': 0,
+        'PPp': 0,
+        'PPs': 0,
+        'PPf': 0
+    }
+
+    for row in rows:
+        pp_metrics['total'] += 1
+        
+        if {'name':'CE 1'} in row['events']:
+            pp_metrics['ce'] += 1
+        elif {'name':'CE 2'} in row['events']:
+            pp_metrics['ce'] += 1
+        elif {'name':'CE 3'} in row['events']:
+            pp_metrics['ce'] += 1
+        elif {'name':'CE 4'} in row['events']:
+            pp_metrics['ce'] += 1
+        elif {'name':'CE 5'} in row['events']:
+            pp_metrics['ce'] += 1
+        
+        if {'name':'GSO'} in row['events']:
+            pp_metrics['gso'] += 1
+        if {'name':'PC Win'} in row['events']:
+            pp_metrics['pc_win'] += 1
+        if {'name':'PP Win '} in row['events']:
+            pp_metrics['pp_win'] += 1
+        if {'name':'PP Win'} in row['events']:
+            pp_metrics['pp_win'] += 1
+        if {'name':'Lost'} in row['events']:
+            pp_metrics['lost'] += 1
+        if {'name':'Foul Won'} in row['events']:
+            pp_metrics['lost'] += 1
+        if {'name':'End of Phase'} in row['events']:
+            pp_metrics['lost'] += 1
+        if {'name':'Goal'} in row['events']:
+            pp_metrics['goal'] += 1
+            pp_metrics['gso'] += 1
+
+    return pp_metrics
+
+def calcMetrics(home_basic_metrics, away_basic_metrics, home_pp_metrics, away_pp_metrics):
+    home_pp_metrics['PPp'] = round((home_pp_metrics['ce'] + home_pp_metrics['gso'] + home_pp_metrics['pc_win'] + home_pp_metrics['goals']) / home_pp_metrics['total'], 2)
+    home_pp_metrics['PPs'] = round((home_pp_metrics['gso'] + home_pp_metrics['pc_win'] + home_pp_metrics['goals']) / home_pp_metrics['total'], 2)
+    home_pp_metrics['PPf'] = round(home_pp_metrics['lost'] / home_pp_metrics['total'], 2)
+
+    away_pp_metrics['PPp'] = round((away_pp_metrics['ce'] + away_pp_metrics['gso'] + away_pp_metrics['pc_win'] + away_pp_metrics['goals']) / away_pp_metrics['total'], 2)
+    away_pp_metrics['PPs'] = round((away_pp_metrics['gso'] + away_pp_metrics['pc_win'] + away_pp_metrics['goals']) / away_pp_metrics['total'], 2)
+    away_pp_metrics['PPf'] = round(away_pp_metrics['lost'] / away_pp_metrics['total'], 2)
+
+    home_basic_metrics['oEff'] = (home_basic_metrics['pp_win'] + home_pp_metrics['pp_win']) * 2 + (sum(home_basic_metrics['25e'].values())) * 2 + (sum(home_basic_metrics['ce'].values()) + home_pp_metrics['ce']) * 4 + (home_basic_metrics['gso'] + home_pp_metrics['gso']) * 6 + (home_basic_metrics['pc_win'] + home_pp_metrics['pc_win']) * 6 + (home_basic_metrics['goals'] + home_pp_metrics['goals']) * 10 - home_basic_metrics['lost'] - home_pp_metrics['lost']
+
+    away_basic_metrics['oEff'] = (away_basic_metrics['pp_win'] + away_pp_metrics['pp_win']) * 2 + (sum(away_basic_metrics['25e'].values())) * 2 + (sum(away_basic_metrics['ce'].values()) + away_pp_metrics['ce']) * 4 + (away_basic_metrics['gso'] + away_pp_metrics['gso']) * 6 + (away_basic_metrics['pc_win'] + away_pp_metrics['pc_win']) * 6 + (away_basic_metrics['goals'] + away_pp_metrics['goals']) * 10 - away_basic_metrics['lost'] - away_pp_metrics['lost']
+
+    home_basic_metrics['25Eff'] = (home_basic_metrics['pp_win'] + home_pp_metrics['pp_win']) + (sum(home_basic_metrics['ce'].values()) + home_pp_metrics['ce']) * 3 + (home_basic_metrics['gso'] + home_pp_metrics['gso']) * 6 + (home_basic_metrics['pc_win'] + home_pp_metrics['pc_win']) * 6 + (home_basic_metrics['goals'] + home_pp_metrics['goals']) * 10 - home_basic_metrics['lost'] - home_pp_metrics['lost']
+
+    away_basic_metrics['25Eff'] = (away_basic_metrics['pp_win'] + away_pp_metrics['pp_win']) + (sum(away_basic_metrics['ce'].values()) + away_pp_metrics['ce']) * 3 + (away_basic_metrics['gso'] + away_pp_metrics['gso']) * 6 + (away_basic_metrics['pc_win'] + away_pp_metrics['pc_win']) * 6 + (away_basic_metrics['goals'] + away_pp_metrics['goals']) * 10 - away_basic_metrics['lost'] - away_pp_metrics['lost']
+
+    home_basic_metrics['ceEff'] = (home_basic_metrics['gso'] + home_pp_metrics['gso']) * 6 + (home_basic_metrics['pc_win'] + home_pp_metrics['pc_win']) * 6 + (home_basic_metrics['goals'] + home_pp_metrics['goals']) * 10 - ((home_basic_metrics['lost'] + home_pp_metrics['lost']) * 2)
+
+    away_basic_metrics['ceEff'] = (away_basic_metrics['gso'] + away_pp_metrics['gso']) * 6 + (away_basic_metrics['pc_win'] + away_pp_metrics['pc_win']) * 6 + (away_basic_metrics['goals'] + away_pp_metrics['goals']) * 10 - ((away_basic_metrics['lost'] + away_pp_metrics['lost']) * 2)
+
+    return (home_basic_metrics, away_basic_metrics, home_pp_metrics, away_pp_metrics)
